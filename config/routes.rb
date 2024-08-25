f0c22b4e6b617fe2e7118d8c757f1626e81d7054
sayabe22 <sayabe22@gmail.com>
2024-08-25 03:54:03 +0000

    [Add]post model

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  get 'users/mypage'
-  get 'users/edit'
-  get 'users/show'
   devise_for :users
-  
+
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
   
   root to: "homes#top"
+  get 'user/mypage' => 'users#mypage',as: :'mypage'
   get 'home/about' => 'homes#about',as: 'about'
+  resources :users
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
