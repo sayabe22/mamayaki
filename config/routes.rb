2c2ebbca4785dbbefbc4ff969dfdf4ea082016f1
sayabe22 <sayabe22@gmail.com>
2024-08-25 01:45:13 +0000

    [Add]top about

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'homes/top'
+  
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  
+  root to: "homes#top"
   get 'home/about' => 'homes#about',as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
