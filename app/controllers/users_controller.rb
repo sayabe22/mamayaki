ab11ef63c9fe2ed89308f24ac51ed315af46f96f
sayabe22 <sayabe22@gmail.com>
2024-08-28 13:41:35 +0000

    [Add]user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < ApplicationController
   def mypage
     @user = current_user
+    @posts = @user.posts
   end
 
   def edit
+    @user = User.find(params[:id])
+    unless @user.id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
   end
 
-  def show
-  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :profile, :is_active, :email, :encrypted_password)
+  end 
+  
 end
