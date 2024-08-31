65fb1c741a4ec02d6a4151c328a17eb113c68591
sayabe22 <sayabe22@gmail.com>
2024-08-31 07:15:14 +0000

    [Add]ユーザー画像

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:user).permit(:name, :profile, :is_active, :email, :encrypted_password)
+    params.require(:user).permit(:name, :profile, :is_active, :email, :encrypted_password, :profile_image)
   end 
   
 end
