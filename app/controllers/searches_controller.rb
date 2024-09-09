c12f733fd45fb6bef3efad72bc9bb9c6bbd64b46
sayabe22 <sayabe22@gmail.com>
2024-09-09 12:13:19 +0000

    Remove記述修正

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,9 +2,10 @@ class SearchesController < ApplicationController
   before_action :authenticate_user!
 
   def search
+    @user = current_user
     @range = params[:range]
 
-    if @range == "User"
+    if @range == "ユーザー"
       @users = User.looks(params[:search], params[:word])
     else
       @posts = Post.looks(params[:search], params[:word])
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,13 +4,13 @@ class Post < ApplicationRecord
   
   def self.looks(search, word)
     if search == "perfect_match"
-      @post = Post.where("title LIKE?","#{word}")
+      @post = Post.where("body LIKE?","#{word}")
     elsif search == "forward_match"
-      @post = Post.where("title LIKE?","#{word}%")
+      @post = Post.where("body LIKE?","#{word}%")
     elsif search == "backward_match"
-      @post = Post.where("title LIKE?","%#{word}")
+      @post = Post.where("body LIKE?","%#{word}")
     elsif search == "partial_match"
-      @post = Post.where("title LIKE?","%#{word}%")
+      @post = Post.where("body LIKE?","%#{word}%")
     else
       @post = Post.all
     end
