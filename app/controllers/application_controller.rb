0078f5daba8c7c9aad327b65fcc5994be3127d61
sayabe22 <sayabe22@gmail.com>
2024-09-11 07:04:04 +0000

    Add admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,22 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def after_sign_in_path_for(resource)
-    mypage_path
+  before_action :configure_authentication
+  
+  
+  private
+  
+  def configure_authentication
+    if admin_controller?
+      authenticate_admin!
+    end
+  end
+ 
+  def admin_controller?
+    self.class.module_parent_name == 'Admin'
+  end
+ 
+  def action_is_public?
+    controller_name == 'homes' && action_name == 'top'
   end
   
-  def after_sign_out_path_for(resource)
-    root_path
-  end
   
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
 end
