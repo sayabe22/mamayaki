bbb3226ca5e8b9ff1c0e76deb04631af3cf8955f
sayabe22 <sayabe22@gmail.com>
2024-09-12 02:35:01 +0000

    Remove コメント件数表示

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -9,7 +9,7 @@ class Public::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.page(params[:page])
+    @posts = @user.posts.page(params[:page]).order("created_at DESC")
   end 
 
   def edit
