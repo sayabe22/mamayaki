0a9c9169a03a2991206e030ccee2929a1b40a67b
sayabe22 <sayabe22@gmail.com>
2024-09-12 14:00:52 +0000

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,31 +6,31 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-みか = User.find_or_create_by!(email: "mika@example.com") do |user|
-  user.name = "みか"
+mika = User.find_or_create_by!(email: "mika@example.com") do |user|
+  user.name = "mika"
   user.password = "password"
   user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user1.jpg"), filename:"sample-user1.jpg")
 end
 
-けい = User.find_or_create_by!(email: "kei@example.com") do |user|
-  user.name = "けい"
+kei = User.find_or_create_by!(email: "kei@example.com") do |user|
+  user.name = "kei"
   user.password = "password"
   user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user2.jpg"), filename:"sample-user2.jpg")
 end
 
-ままんぬ = User.find_or_create_by!(email: "mamanmu@example.com") do |user|
+ ままんぬ= User.find_or_create_by!(email: "mamanmu@example.com") do |user|
   user.name = "ままんぬ"
   user.password = "password"
   user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-user3.jpg"), filename:"sample-user3.jpg")
 end
 
 Post.find_or_create_by!(title: "8月30日") do |post|
-  post.body = "今日も暑いですね。皆さんはお子さんの熱中症対策どうしていますか。"
+  post.body = "今日も暑いですね。皆さんはお子さんの熱中症対策どんなことしていますか。"
   post.user_id = 1
 end
 
-Post.find_or_create_by!(title: "共感者求む") do |post|
-  post.body = "４歳になる息子が全く言うことを聞いてくれません。どなたか良い解決策お持ちでないですか。"
+Post.find_or_create_by!(title: "遊園地へ") do |post|
+  post.body = "本日は家族で遊園地へ。子供が喜ぶ姿を見ると癒されますね。"
   post.user_id = 2
 end
 
@@ -38,3 +38,11 @@ Post.find_or_create_by!(title: "先日見かけた光景")  do |post|
   post.body = "小さい子供にリールをつけている方がたまにいますがみなさんどのように思いますか。"
   post.user_id = 3
 end
+
+PostComment.find_or_create_by!(comment: "クルーネックリングは必須ですね。", user_id: 3, post_id: 1)
+
+PostComment.find_or_create_by!(comment: "我が家はハンディタイプの扇風機を持ち歩いています。", user_id: 2, post_id: 1)
+
+PostComment.find_or_create_by!(comment: "子どもの安全のためなので一概に否定はできないですよね～", user_id: 2, post_id: 3)
+
+Admin.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
