8088bf2089cabd3764a3a59b908893897682a21c
sayabe22 <sayabe22@gmail.com>
2024-09-14 02:24:14 +0000

    Add favorite

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -1,17 +1,16 @@
 class Public::PostCommentsController < ApplicationController
   
   def create
-    post = Post.find(params[:post_id])
+    @post = Post.find(params[:post_id])
+    @user = @post.user
+    @post_comment = PostComment.new
     comment = current_user.post_comments.new(post_comment_params)
-    comment.post_id = post.id
+    comment.post_id = @post.id
     if comment.save
       flash[:notice] = "コメントしました。"
-      redirect_to post_path(post)
+      redirect_to post_path(@post)
     else
       @error_comment = comment
-      @post = Post.find(params[:post_id])
-      @user = @post.user
-      @post_comment = PostComment.new
       render 'public/posts/show'
     end
   end
