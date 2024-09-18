e75c7370f12acea2d509dc14c61e034d14d02d33
sayabe22 <sayabe22@gmail.com>
2024-09-18 12:14:21 +0000

    Update ゲストログイン時全ユーザー投稿表示

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -19,8 +19,12 @@ class Public::PostsController < ApplicationController
   end 
 
   def index
-    @posts = Post.page(params[:page]).order("created_at DESC")
     @user = current_user
+    if @user.email == 'guest@example.com'
+       @posts = Post.page(params[:page]).order("created_at DESC")
+    else
+      @posts = Post.page(params[:page]).order("created_at DESC").where(user_id: [current_user.id, *current_user.following_ids])
+    end
   end
 
   def show
