fe5999e533547dc97ec9e55662e37f81d8d45e17
sayabe22 <sayabe22@gmail.com>
2024-09-19 12:22:34 +0000

    Add コメント通知

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -3,8 +3,10 @@ module NotificationsHelper
     case notification.notifiable_type
     when "Post"
       "フォローしている#{notification.notifiable.user.name}さんが#{notification.notifiable.title}を投稿しました"
-    else
+    when "Favorite"
       "投稿した#{notification.notifiable.post.title}が#{notification.notifiable.user.name}さんにいいねされました"
+    else "Post_comment"
+      "投稿した#{notification.notifiable.post.title}が#{notification.notifiable.user.name}さんにコメントされました"
     end
   end
 end
--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -4,6 +4,10 @@ class PostComment < ApplicationRecord
   belongs_to :post
   has_one :notification, as: :notifiable, dependent: :destroy
   
+  after_create do
+    create_notification(user_id: post.user_id)
+  end  
+  
   validates :comment, presence: true
     
 end
